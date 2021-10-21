0db3cb48dbace23dbc3f5a0db740ae47746244dd
chekuthan <chekuthankl13@gmail.com>
2021-10-21 10:38:05 +0530

    floatiing icontab

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:temple/sections/about.dart';
 import 'package:temple/sections/buttontab.dart';
@@ -8,7 +9,6 @@ import 'package:temple/sections/offerings.dart';
 import 'package:temple/sections/photos.dart';
 import 'package:temple/sections/update.dart';
 import 'package:temple/sections/videos.dart';
-
 import 'package:temple/widgets/Buttons/icons.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -19,90 +19,122 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late final ScrollController _scrollController;
+
+  bool sliverPersistentHeader = false;
   int? value;
+
+
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+    _scrollController.addListener(() {
+      if (_scrollController.position.userScrollDirection ==
+          ScrollDirection.reverse) {
+        setState(() {
+          sliverPersistentHeader = false;
+        });
+      } else {
+        setState(() {
+          sliverPersistentHeader = true;
+        });
+      }
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     var red = Colors.blue.shade50;
     var white = const Color(0xFFF2F2F2);
     return Scaffold(
-      // backgroundColor: Colors.amber,
-      body: CustomScrollView(slivers: [
+      body: CustomScrollView(controller: _scrollController, slivers: [
         SliverPersistentHeader(
           pinned: true,
           delegate: CustomSliverAppBarDelegate(expandedHeight: 335),
         ),
+        SliverPersistentHeader(
+            pinned: sliverPersistentHeader ? true : false,
+            delegate: PersistentButton(
+              widget: ButtonTabs(
+                children: [
+                  ButtonIcon(
+                    clr: value == 0 ? red : white,
+                    onPressed: () {
+                      setState(() {
+                        value = 0;
+                      });
+                    },
+                    icons: SvgPicture.asset('assets/icons/Update.svg'),
+                    txt: 'Update',
+                  ),
+                  ButtonIcon(
+                    clr: value == 1 ? red : Colors.white,
+                    onPressed: () {
+                      setState(() {
+                        value = 1;
+                      });
+                    },
+                    icons: SvgPicture.asset('assets/icons/Prathista.svg'),
+                    txt: 'Deities',
+                  ),
+                  ButtonIcon(
+                    clr: value == 2 ? red : Colors.white,
+                    onPressed: () {
+                      setState(() {
+                        value = 2;
+                      });
+                    },
+                    icons: SvgPicture.asset('assets/icons/Offering.svg'),
+                    txt: 'Offerings',
+                  ),
+                  ButtonIcon(
+                    clr: value == 3 ? red : Colors.white,
+                    onPressed: () {
+                      setState(() {
+                        value = 3;
+                      });
+                    },
+                    icons: SvgPicture.asset('assets/icons/Contact.svg'),
+                    txt: 'About',
+                  ),
+                  ButtonIcon(
+                    clr: value == 4 ? red : Colors.white,
+                    onPressed: () {
+                      setState(() {
+                        value = 4;
+                      });
+                    },
+                    icons: SvgPicture.asset('assets/icons/image-line.svg'),
+                    txt: 'photos',
+                  ),
+                  ButtonIcon(
+                    clr: value == 5 ? red : Colors.white,
+                    onPressed: () {
+                      setState(() {
+                        value = 5;
+                      });
+                    },
+                    icons: SvgPicture.asset('assets/icons/video.svg'),
+                    txt: 'videos',
+                  ),
+                  const SizedBox(
+                    width: 15,
+                  )
+                ],
+              ),
+            ),
+            ),
         SliverToBoxAdapter(
           child: SingleChildScrollView(
             child: Column(
               children: [
-                ButtonTabs(
-                  children: [
-                    ButtonIcon(
-                      clr: value == 0 ? red : white,
-                      onPressed: () {
-                        setState(() {
-                          value = 0;
-                        });
-                      },
-                      icons: SvgPicture.asset('assets/icons/Update.svg'),
-                      txt: 'Update',
-                    ),
-                    ButtonIcon(
-                      clr: value == 1 ? red : white,
-                      onPressed: () {
-                        setState(() {
-                          value = 1;
-                        });
-                      },
-                      icons: SvgPicture.asset('assets/icons/Prathista.svg'),
-                      txt: 'Deities',
-                    ),
-                    ButtonIcon(
-                      clr: value == 2 ? red : white,
-                      onPressed: () {
-                        setState(() {
-                          value = 2;
-                        });
-                      },
-                      icons: SvgPicture.asset('assets/icons/Offering.svg'),
-                      txt: 'Offerings',
-                    ),
-                    ButtonIcon(
-                      clr: value == 3 ? red : white,
-                      onPressed: () {
-                        setState(() {
-                          value = 3;
-                        });
-                      },
-                      icons: SvgPicture.asset('assets/icons/Contact.svg'),
-                      txt: 'About',
-                    ),
-                    ButtonIcon(
-                      clr: value == 4 ? red : white,
-                      onPressed: () {
-                        setState(() {
-                          value = 4;
-                        });
-                      },
-                      icons: SvgPicture.asset('assets/icons/image-line.svg'),
-                      txt: 'photos',
-                    ),
-                    ButtonIcon(
-                      clr: value == 5 ? red : white,
-                      onPressed: () {
-                        setState(() {
-                          value = 5;
-                        });
-                      },
-                      icons: SvgPicture.asset('assets/icons/video.svg'),
-                      txt: 'videos',
-                    ),
-                    const SizedBox(
-                      width: 15,
-                    )
-                  ],
-                ),
                 value == 0
                     ? const Updates()
                     : value == 1
@@ -171,4 +203,26 @@ class CustomSliverAppBarDelegate extends SliverPersistentHeaderDelegate {
         opacity: disappear(shrinkOffset),
         child: const Header(),
       );
+}
+
+class PersistentButton extends SliverPersistentHeaderDelegate {
+  final Widget widget;
+  PersistentButton({required this.widget});
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return widget;
+  }
+
+  @override
+  double get maxExtent => 110;
+
+  @override
+  double get minExtent => 110;
+
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    return true;
+  }
 }
